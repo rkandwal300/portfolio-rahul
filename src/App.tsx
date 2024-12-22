d74fcf392982611913a8868a0f746fb81859e3bd
Rahul Kandwal <rahulkandwal707@gmail.com>
2024-12-22 22:02:22 +0530

    added  react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import PostDetail from './components/shared/blog/PostDetail';
+import PostsList from './components/shared/blog/PostsList';
 import Header from './components/shared/Header';
 import Home from './components/shared/Home';
+import ProjectList from './components/shared/project/ProjectList';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Header /> <Home />
+        </>
+      ),
+    },
+    {
+      path: '/projects',
+      element: (
+        <>
+          {' '}
+          <Header /> <ProjectList />
+        </>
+      ),
+    },
+    {
+      path: '/blog',
+      element: (
+        <>
+          <Header />
+          <PostsList />
+        </>
+      ),
+    },
+    {
+      path: '/blog/:blogId',
+      element: (
+        <>
+          <Header />
+          <PostDetail />
+        </>
+      ),
+    },
+    {
+      path: '/contact',
+      element: (
+        <>
+          <Header />
+          <ProjectList />
+        </>
+      ),
+    },
+  ]);
   return (
     <section className="max-w-3xl px=-8 mx-auto min-h-screen font-sans w-full  grow antialiased __variable_36bd41 __variable_33d430">
-      <Header />
-      <Home />
+      <RouterProvider router={router} />
     </section>
   );
 }
