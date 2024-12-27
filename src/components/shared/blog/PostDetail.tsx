91a38d4cd07c26e464c158e808eb39d2c74f07b6
Rahul Kandwal <rahulkandwal707@gmail.com>
2024-12-27 22:27:59 +0530

    [blog detail]: added iframe

--- src/components/shared/blog/PostDetail.tsx
+++ src/components/shared/blog/PostDetail.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, SquareArrowOutUpRight } from 'lucide-react';
 import { Link, useParams } from 'react-router-dom';
 import { posts } from '../../../lib/data';
 import { cn } from '../../../lib/utils';
@@ -8,33 +8,49 @@ export default function PostDetail() {
   const singleBlog = posts.find((post) => post.id === params.blogId);
   return (
     <section className="flex flex-col gap-4 mt-8 pb-16 grow ">
-      <Link
-        to="/blog"
-        className="text-muted-foreground hover:text-foreground flex items-center gap-2 font-light"
-      >
-        <ArrowLeft size={16} />
-        <span>back to log</span>
-      </Link>
-      <div className="relative mb-6 h-96 w-full overflow-hidden rounded-lg">
-        <img
-          alt={singleBlog?.title}
-          loading="lazy"
-          decoding="async"
-          data-nimg="fill"
-          className="object-cover"
-          sizes="100vw"
-          src={singleBlog?.link}
-          style={{
-            position: 'absolute',
-            height: '100%',
-            width: '100%',
-            inset: '0px',
-            color: 'transparent',
-          }}
-        />
+      <div className="flex justify-between py-2">
+        <Link
+          to="/blog"
+          className="text-muted-foreground hover:text-foreground flex items-center gap-2 font-light"
+        >
+          <ArrowLeft size={16} />
+          <span>back to log</span>
+        </Link>
+        <Link
+          to={singleBlog?.link ?? ''}
+          className="text-muted-foreground hover:text-foreground flex items-center gap-2 font-light"
+        >
+          <SquareArrowOutUpRight size={16} />
+          <span>Open</span>
+        </Link>
       </div>
-      <div className="flex flex-col">
-        <h2 className={cn('font-calistoga', length ? 'text-2xl' : 'text-5xl')}>
+      <div className="relative mb-6 h-96 w-full overflow-hidden rounded-lg">
+        <div id="wrap">
+          <iframe
+            id="scaled-frame"
+            title={singleBlog?.title}
+            loading="lazy"
+            className="object-cover"
+            src={singleBlog?.link}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              height: '100%',
+              width: '100%',
+              border: 'none',
+              transformOrigin: 'top left',
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col tracking-wider">
+        <h2
+          className={cn(
+            'font-calistoga title',
+            length ? 'text-2xl' : 'text-5xl'
+          )}
+        >
           {singleBlog?.title}
         </h2>
         <p className="text-xs mt-1.5">{singleBlog?.date}</p>
